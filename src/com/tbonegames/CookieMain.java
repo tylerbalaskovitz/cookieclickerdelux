3c9c365108ff4d3b2a838980b619e8d6610082d3
tylerbalaskovitz <tylerbalaskovitz@yahoo.com>
2022-08-10 17:00:45 -0400

    Pushing

---
+++ src/com/tbonegames/CookieMain.java
@@ -0,0 +1,378 @@
+package com.tbonegames;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.Timer;
+
+public class CookieMain {
+
+	JLabel counterLabel, perSecLabel;
+	JButton button1, button2, button3, button4;
+	int cookieCounter, timerSpeed, cursorNumber, cursorPrice, grandpaNumber, grandpaPrice, grandmaPrice, grandmaNumber, elvesPrice, elvesNumber;
+	double perSecond;
+	boolean timerOn, grandpaUnlocked, grandmaUnlocked, elvesUnlocked;
+	Font font1, font2;
+	CookieHandler cHandler = new CookieHandler();
+	Timer timer;
+	JTextArea messageText;
+	MouseHandler mHandler = new MouseHandler();
+	
+	public static void main(String[] args) {
+		new CookieMain();
+	}
+	
+	public CookieMain() {
+		
+		grandpaUnlocked = false;
+		timerOn = false;
+		perSecond = 0;
+		cookieCounter = 0;
+		grandpaPrice = 100;
+		grandmaPrice = 200;
+		elvesPrice = 500;
+		cursorNumber = 0;
+		cursorPrice = 10;
+		
+		createFont();
+		
+		createUI();
+		
+	}
+	
+	public void createFont() {
+		
+		font1 = new Font("Comic Sans MS", Font.PLAIN, 32);
+		font2 = new Font("Comic Sans MS", Font.PLAIN, 16);
+		
+	}
+	
+	
+	public void createUI() {
+		
+		JFrame window = new JFrame();
+		window.setSize(800, 600);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.getContentPane().setBackground(Color.black);
+		window.setLayout(null);
+		
+		JPanel cookiePanel = new JPanel();
+		cookiePanel.setBounds(100, 220, 200, 200);
+		cookiePanel.setBackground(Color.black);
+		window.add(cookiePanel);
+		
+		
+		ImageIcon cookie = new ImageIcon(getClass().getClassLoader().getResource("cookie.png"));
+		
+		JButton cookieButton = new JButton();
+		
+		
+		cookieButton.setBackground(Color.black);
+		cookieButton.setFocusPainted(false);
+		cookieButton.setBorder(null);
+		cookieButton.addActionListener(cHandler);
+		cookieButton.setActionCommand("cookie");
+		//setIcon is a method that allows you to set a picture on this Button
+		cookieButton.setIcon(cookie);
+		cookiePanel.add(cookieButton);
+		
+		JPanel counterPanel = new JPanel();
+		counterPanel.setBounds(100,100,200,100);
+		counterPanel.setBackground(Color.black);
+		counterPanel.setLayout(new GridLayout(2,1));
+		window.add(counterPanel);
+		
+		counterLabel = new JLabel(cookieCounter + " Cookies");
+		counterLabel.setFont(font1);
+		counterLabel.setForeground(Color.white);
+		counterPanel.add(counterLabel);
+		
+		perSecLabel = new JLabel();
+		perSecLabel.setFont(font2);
+		perSecLabel.setForeground(Color.white);
+		counterPanel.add(perSecLabel);
+		
+		
+		JPanel itemPanel = new JPanel();
+		itemPanel.setBounds(500, 170, 250, 250);
+		itemPanel.setBackground(Color.blue);
+		itemPanel.setLayout(new GridLayout(4,1));
+		
+		
+		button1 = new JButton("Cursor");
+		button1.setFont(font1);
+		button1.setFocusPainted(false);
+		button1.addActionListener(cHandler);
+		button1.setActionCommand("Cursor");
+		button1.addMouseListener(mHandler);
+		itemPanel.add(button1);
+		
+		button2 = new JButton("?");
+		button2.setFont(font1);
+		button2.setFocusPainted(false);
+		button2.addActionListener(cHandler);
+		button2.setActionCommand("Grandpa");
+		button2.addMouseListener(mHandler);
+		itemPanel.add(button2);
+		
+		button3 = new JButton("?");
+		button3.setFont(font1);
+		button3.setFocusPainted(false);
+		button3.addActionListener(cHandler);
+		button3.setActionCommand("Grandma");
+		button3.addMouseListener(mHandler);
+		itemPanel.add(button3);
+		
+		button4 = new JButton("?");
+		button4.setFont(font1);
+		button4.setFocusPainted(false);
+		button4.addActionListener(cHandler);
+		button4.setActionCommand("Elves");
+		button4.addMouseListener(mHandler);
+		itemPanel.add(button4);
+		
+		
+		
+		window.add(itemPanel);
+		
+		JPanel messagePanel = new JPanel();
+		messagePanel.setBounds(500,  70, 250, 150);
+		messagePanel.setBackground(Color.black);
+		window.add(messagePanel);
+		
+		messageText = new JTextArea();
+		messageText.setBounds(500, 70, 250, 150);
+		messageText.setForeground(Color.white);
+		messageText.setBackground(Color.black);
+		messageText.setFont(font2);
+		messageText.setLineWrap(true);
+		messageText.setWrapStyleWord(true);
+		messageText.setEditable(false);
+		messagePanel.add(messageText);
+		
+		
+		
+		window.setVisible(true);
+		
+		
+	}
+	
+	public void setTimer() {
+		timer = new Timer(timerSpeed, new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				cookieCounter++;
+				counterLabel.setText(cookieCounter + " Cookies");
+				
+				if(grandpaUnlocked==false) {
+					if(cookieCounter >= 100) {
+						grandpaUnlocked = true;
+						button2.setText("Grandpa " + "(" + grandpaNumber + ")");
+					}
+				} else if (grandpaUnlocked == true){
+					button2.setText("Grandpa " + "(" + grandpaNumber + ")");
+				}
+				
+				if(grandmaUnlocked==false) {
+					if(cookieCounter >= 200) {
+						grandmaUnlocked = true;
+						button3.setText("Grandma " + "(" + grandmaNumber + ")");
+					}
+				} else if (grandmaUnlocked == true){
+					button3.setText("Grandma " + "(" + grandmaNumber + ")");
+				}
+				
+				if(elvesUnlocked==false) {
+					if(cookieCounter >= 500) {
+						elvesUnlocked = true;
+						button4.setText("Elves " + "(" + elvesNumber + ")");
+					}
+				} else if (elvesUnlocked == true){
+					button4.setText("Elves " + "(" + elvesNumber + ")");
+				}
+				
+				
+			}
+		});
+	}
+	
+	public void timerUpdate() {
+		if (timerOn == false) {
+			
+			timerOn=true;
+		}
+		else if(timerOn==true) {
+			timer.stop();
+		}
+		
+		double speed = 1/perSecond*1000;
+		timerSpeed = (int)Math.round(speed);
+		
+		//the %.1f is a a way for creating a format where the float(decimal) will go 1 digit deep)
+		String s = String.format("%.1f", perSecond);
+		perSecLabel.setText("Per Second: " + s);
+		
+		setTimer();
+		timer.start();
+		
+	}
+	
+	public class CookieHandler implements ActionListener{
+		
+		
+		public void actionPerformed(ActionEvent event) {
+			
+			String action = event.getActionCommand();
+			
+			switch (action) {
+			case "cookie": cookieCounter++; counterLabel.setText(cookieCounter + " Cookies"); break;
+				case "Cursor": if (cookieCounter >= cursorPrice) {
+					cookieCounter = cookieCounter - cursorPrice;
+					cursorPrice = cursorPrice + 5;
+					counterLabel.setText(cookieCounter + " Cookies");
+					messageText.setText("Cursor: \n [Price: " + cursorPrice + "] \n AutoClicks once every 10 seconds");
+					cursorNumber++;
+					button1.setText("Cursor " + "(" + cursorNumber + ")");
+					perSecond = perSecond + 0.1; timerUpdate();
+				} else {
+					messageText.setText("You need more cookies!"); 
+				}
+				break;
+				case "Grandpa":
+				 if (cookieCounter >= grandpaPrice) {
+					cookieCounter = cookieCounter - grandpaPrice;
+					grandpaPrice = grandpaPrice + 50;
+					counterLabel.setText(cookieCounter + " Cookies");
+					grandpaNumber++;
+					messageText.setText("Grandpa: \n [Price: " + grandpaPrice + "] \n Each grandpa produces 1 cookie per second");
+					button2.setText("Grandpa " + "(" + grandpaNumber + ")");
+					perSecond = perSecond + 1; timerUpdate();
+				} else {
+					messageText.setText("You need more cookies!"); 
+				}
+				break;
+				case "Grandma":
+					if (cookieCounter >= grandmaPrice) {
+						cookieCounter = cookieCounter - grandmaPrice;
+						grandmaPrice = grandmaPrice + 200;
+						counterLabel.setText(cookieCounter + " Cookies");
+						grandmaNumber++;
+						messageText.setText("Grandma: \n [Price: " + grandmaPrice + "] \n Each grandma produces 3 cookie per second");
+						button3.setText("Grandpa " + "(" + grandmaNumber + ")");
+						perSecond = perSecond + 3; timerUpdate();
+					} else {
+						messageText.setText("You need more cookies!"); 
+					}
+				break;
+				case "Elves":
+					if (cookieCounter >= elvesPrice) {
+						cookieCounter = cookieCounter - elvesPrice;
+						elvesPrice = elvesPrice + 500;
+						counterLabel.setText(cookieCounter + " Cookies");
+						elvesNumber++;
+						messageText.setText("Elves: \n [Price: " + elvesPrice + "] \n Each elf produces 7 cookie per second");
+						button4.setText("Elves " + "(" + elvesNumber + ")");
+						perSecond = perSecond + 7; timerUpdate();
+					} else {
+						messageText.setText("You need more cookies!"); 
+					}
+				break;
+			
+			}
+		
+		}
+
+	}
+	
+	public class MouseHandler implements MouseListener{
+		
+	
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		JButton button = (JButton)e.getSource();
+		
+		if(button == button1) {
+			messageText.setText("Cursor: \n [Price: " + cursorPrice + "] \n AutoClicks once every 10 seconds");
+		}
+		
+		if(button == button2) {
+			if(grandpaUnlocked==false) {
+			messageText.setText("This item is currently locked");
+			} else {
+				messageText.setText("Grandpa: \n [Price: " + grandpaPrice + "] \n Each grandpa produces 1 cookie per second");
+			}
+		}
+		if(button == button3) {
+			if(grandmaUnlocked==false) {
+				
+			
+			messageText.setText("This item is currently locked");
+			} else {
+			messageText.setText("Grandma: \n [Price: " + grandmaPrice + "] \n Each grandma produces 3 cookie per second");
+			}
+		}
+		
+		if(button == button4) {
+			if(elvesUnlocked==false) {
+			messageText.setText("This item is currently locked");
+			} else {
+				messageText.setText("Elves: \n [Price: " + elvesPrice + "] \n Each elf produces 7 cookie per second");
+			}
+		}
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		JButton button = (JButton)e.getSource();
+		if(button == button1) {
+			messageText.setText(null);
+		}
+		else if (button == button2) {
+			messageText.setText(null);
+		}
+		else if (button == button3) {
+			messageText.setText(null);
+		}
+		else if (button == button4) {
+			messageText.setText(null);
+		}
+		
+	}
+	
+	}
+	
+
+}
+
