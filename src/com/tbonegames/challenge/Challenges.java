8009e06b4c8cffb8a6bb07997beddfa28727e968
tylerbalaskovitz <tylerbalaskovitz@yahoo.com>
2022-11-01 14:54:24 -0400

    Making more progress on the high score table. Another class has been made that is referred to by the Challenges class

--- src/com/tbonegames/challenge/Challenges.java
+++ src/com/tbonegames/challenge/Challenges.java
@@ -3,6 +3,7 @@ package com.tbonegames.challenge;
 import java.io.Serializable;
 
 import com.tbonegames.ClientMain;
+import com.tbonegames.highscore.HighScore;
 
 public class Challenges implements Serializable{
 	final static long serialVersionUID = -1404202925519361557L;
@@ -13,12 +14,65 @@ public class Challenges implements Serializable{
 	
 	boolean completed;
 	
+	//Each challenges high score data
+	
+	public int highScoreMarker;
+	
+	public String tableName;
+	
+	public String playerName;
+	
+	public int highScore;
+	
+	HighScore highScoreTable[] = new HighScore[5];
+	
 	public void combatChallenge(ClientMain cMain) {
 		
 	}
 	
 	public void menuChallenge(ClientMain cMain) {
 		
+	}
+	
+	
+	public String displayHighScoreTable() {
+
+		return highScoreTable.toString();
+	}
+	
+	
+	public void addHighScore(String nameEntry, int newHighScore) {
+		for (int i = 1; i < 5; i++) {
+			if (newHighScore > highScoreTable[0].highScore) {
+				highScoreMarker = 0;
+				for (int j = 3; j >= 0; j--) {
+					highScoreTable[j+1].highScore = highScoreTable[j].highScore;
+					highScoreTable[j+1].playerName = highScoreTable[j].playerName;
+				}
+				highScoreTable[0].playerName = nameEntry;
+				highScoreTable[0].highScore = newHighScore;
+				break;
+				
+			} else if ( highScoreTable[i-1].highScore > newHighScore && newHighScore > highScoreTable[i].highScore ) {
+				int heldValue = 5 - i;
+				
+				for (int k = 4; k > heldValue; k--) {
+					highScoreTable[k+1].highScore = highScoreTable[k].highScore;
+					highScoreTable[k+1].playerName = highScoreTable[k].playerName;
+				}
+				
+				highScoreTable[i].highScore = newHighScore;
+				highScoreTable[i].playerName = nameEntry;
+			}
+		}
+	}
+	
+	public void generateHighScore() {
+		for (int i = 0; i < 5; i++) {
+		
+			highScoreTable[i].playerName = "John Doe"; 
+			highScoreTable[i].highScore = 10000*i;
+		}
 	}
 
 }
--- src/com/tbonegames/highscore/HighScore.java
+++ src/com/tbonegames/highscore/HighScore.java
@@ -11,46 +11,7 @@ public class HighScore {
 	
 	public int highScore;
 	
-	HighScore highScoreTable[] = new HighScore[5];
 	
-	public String displayHighScoreTable() {
 
-		return highScoreTable.toString();
-	}
-	
-	
-	public void addHighScore(String nameEntry, int newHighScore) {
-		for (int i = 1; i < 5; i++) {
-			if (newHighScore > highScoreTable[0].highScore) {
-				highScoreMarker = 0;
-				for (int j = 3; j >= 0; j--) {
-					highScoreTable[j+1].highScore = highScoreTable[j].highScore;
-					highScoreTable[j+1].playerName = highScoreTable[j+1].playerName;
-				}
-				highScoreTable[0].playerName = nameEntry;
-				highScoreTable[0].highScore = newHighScore;
-				break;
-				
-			} else if (newHighScore > highScoreTable[i].highScore && newHighScore < highScoreTable[i-1].highScore) {
-				
-				for (int heldValue = i; 1+heldValue <5; heldValue++) {
-					
-				}
-				
-				highScoreTable[i].highScore = newHighScore;
-				highScoreTable[i].playerName = nameEntry;
-			}
-		}
-	}
-	
-	public void generateHighScore() {
-		for (int i = 0; i < 5; i++) {
-		
-			highScoreTable[i].playerName = "John Doe"; 
-			highScoreTable[i].highScore = 10000*i;
-		}
-	}
-	
-	
 	
 }
